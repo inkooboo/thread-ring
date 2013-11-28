337b28c3c45386f7f71860d233e60590495ff092
inkooboo <inkooboo@inkooboo-Lenovo-IdeaPad-S400.(none)>
2013-11-29 01:37:21 +0400

    very slow

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#define _GNU_SOURCE
-
 #include <sched.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <atomic>
 
 enum
 {
@@ -14,7 +13,8 @@ enum
 
 pthread_t threads[THREAD_COUNT];
 char stacks[THREAD_COUNT][STACK_SIZE];
-int mailboxes[THREAD_COUNT];
+int indexes[THREAD_COUNT];
+std::atomic<int> mailboxes[THREAD_COUNT];
 
 int max_token = 0;
 int num_cores = 1;
@@ -22,23 +22,24 @@ int num_cores = 1;
 void * thread_func(void *param)
 {
     const int index = *(int *)param;
-    int *mailbox  = mailboxes + index;
-    int *next_mailbox = mailboxes + ((index + 1) % THREAD_COUNT);
+    std::atomic<int> *mailbox  = mailboxes + index;
+    std::atomic<int> *next_mailbox = mailboxes + ((index + 1) % THREAD_COUNT);
 
+    const int cpu = index * num_cores / THREAD_COUNT;
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
-    CPU_SET(0, &cpuset);
+    CPU_SET(cpu, &cpuset);
     pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 
     for (;;) {
-        const int token = *mailbox;
+        const int token = mailbox->load(std::memory_order_relaxed);
         if (!token) {
-            usleep(1);
+            pthread_yield();
             continue;
         }
 
-        *next_mailbox = token + 1;
-        *mailbox = 0;
+        next_mailbox->store(token + 1, std::memory_order_relaxed);
+        mailbox->store(0, std::memory_order_relaxed);
 
         if (token == max_token) {
             printf("%d\n", index + 1);
@@ -52,30 +53,27 @@ void * thread_func(void *param)
     return 0;
 }
 
-int main(int argc, char *argv[])
+int main(int, char *argv[])
 {
-    int i = 0;
-
-    (void)argc;
-
     max_token = strtol(argv[1], NULL, 10);
 
     num_cores = sysconf(_SC_NPROCESSORS_ONLN);
 
-    for (i = 0; i < THREAD_COUNT; ++i) {
-        mailboxes[i] = 0;
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        mailboxes[i].store(0);
     }
-    mailboxes[0] = 1;
+    mailboxes[0].store(1);
 
     pthread_attr_t stack_attr;
     pthread_attr_init(&stack_attr);
 
-    for (i = 0; i < THREAD_COUNT; ++i) {
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+	indexes[i] = i;
         pthread_attr_setstack(&stack_attr, &stacks[i], STACK_SIZE);
-        pthread_create(&threads[i], &stack_attr, &thread_func, (void *)i);
+        pthread_create(&threads[i], &stack_attr, &thread_func, (void *)(indexes + i));
     }
 
-    pthread_join(threads[max_token % THREAD_COUNT], NULL);
+    pthread_join(threads[(max_token - 1) % THREAD_COUNT], NULL);
 
     return 0;
 }
